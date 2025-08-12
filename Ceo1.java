bc19545032314d26c55e4fb83865e27730197b1b
megha6361 <lmeghana635@gmail.com>
2025-08-12 11:31:29 +0530

    creating files

---
+++ Ceo1.java
@@ -0,0 +1,26 @@
+class Ceo1{
+      
+
+    private String name;
+    private float salary;
+    private String city;
+
+    public Ceo1(String name, float salary, String city){
+        this.name = name;
+        this.salary = salary;
+        this.city = city;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public float getSalary(){
+        return salary;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    }
